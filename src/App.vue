481b2a23ca79ff06a28a415f54364ce19ac0e9be
Alexey <dis320852@gmail.com>
2023-11-09 13:43:12 +0200

    create

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Index from './components/Index.vue'
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  <Index />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+// defineProps({
+//   msg: String,
+// })
+
+</script>
+
+<template>
+<div>
+  <div class="m-4 text-center">
+   <a class="mx-12 hover:underline hover:text-fuchsia-200" href="#">Home</a>
+   <a class="mx-12 hover:underline hover:text-fuchsia-200" href="#">About</a>
+   <a class="mx-12 hover:underline hover:text-fuchsia-200" href="#">Catalog</a>
+   <a class="mx-12 hover:underline hover:text-fuchsia-200" href="#">Setting</a>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+</style>
